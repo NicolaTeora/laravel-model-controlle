99baea5d42399894c5f07f5617d34c76fccbbbaa
NicolaTeora <nikxodia@gmail.com>
2024-03-19 18:47:59 +0100

    feat : dev function index() and print in dom

--- app/Http/Controllers/Guest/MoviePageController.php
+++ app/Http/Controllers/Guest/MoviePageController.php
@@ -5,15 +5,20 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+//ricordare sempre di importare lo "use" per utilizzare il modello
 use App\Models\Movie;
 
 class MoviePageController extends Controller
 {
-
+    /**
+     * All'interno della funzione index() del controller, recuperate tutti i film dal database
+     *  e passateli alla view, che quindi li visualizzerà a schermo, tramite delle card.
+     */
     public function index()
     {
         //creo una collezione di Movies mettendola in una variabile
         $movies = Movie::all();
-        return view('list-movie');
+
+        return view('list-movie', compact('movies'));
     }
 }
--- resources/views/list-movie.blade.php
+++ resources/views/list-movie.blade.php
@@ -3,9 +3,18 @@
 @section('title', '- movie list')
 
 @section('main-content')
-
+@dump($movies)
     <div class="container">
-        qui vanno i film
+        <div class="row g-2">
+            @foreach ($movies as $movie)
+                <div class="col-4">
+                    <div class="card h-100 p-2">
+                        <h4>{{$movie->original_title}}</h4>
+                    </div>
+                </div>
+            @endforeach
+
+        </div>
     </div>
     
 @endsection
